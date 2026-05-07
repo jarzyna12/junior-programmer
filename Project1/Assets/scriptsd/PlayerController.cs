6adf43a3c5212a3afcc889565a831ffada5cf33c
jarzyna12 <jarzynana@gmail.com>
2026-05-07 21:17:02 +0200

    dodano skrypt poruszania się kamery za graczem, dodano możliwość skręcania pojazdem

--- Project1/Assets/scriptsd/PlayerController.cs
+++ Project1/Assets/scriptsd/PlayerController.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
